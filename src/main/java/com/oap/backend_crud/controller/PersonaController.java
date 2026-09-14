172f29413c0f0fec1fcd597092c42d4597fda23c
JhimySam <jhimysam@gmail.com>
2026-09-14 17:33:57 -0400

    optimizando las clases con lombook

--- src/main/java/com/oap/backend_crud/controller/PersonaController.java
+++ src/main/java/com/oap/backend_crud/controller/PersonaController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     private final PersonaService personaService;
 
